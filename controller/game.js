a00b02b08af442aa32886beb7590ebb582eda4d6
jurgob <jurgo.boemo@gmail.com>
2015-07-30 01:02:06 +0100

    code clean

--- controller/game.js
+++ controller/game.js
@@ -1,8 +1,6 @@
 var Game = require('../models/game');
 
-// Create endpoint /api/beers for POST
 exports.postGame = function(req, res) {
-  // Create a new instance of the Beer model
   var game = new Game();
   game.word2Guess = 'Secret';
   game.alphabet = "abcdefghilmnopqrstuvwxyz";
@@ -18,7 +16,6 @@ exports.postGame = function(req, res) {
 };
 
 exports.getGames = function(req, res) {
-  // Use the Beer model to find all beer
   Game.find({}, function(err, game) {
     if (err)
       res.send(err);
@@ -28,7 +25,6 @@ exports.getGames = function(req, res) {
 };
 
 exports.getGame = function(req, res) {
-  // Use the Beer model to find all beer
   Game.find({_id: req.params.game_id}, function(err, game) {
     if (err)
       res.send(err);
@@ -37,15 +33,10 @@ exports.getGame = function(req, res) {
   });
 };
 
-
 exports.postMove = function(req, res) {
-  // Use the Beer model to find all beer
-  // :move
-  //[ "$item", " - ", "$description" ]
   Game.find({_id: req.params.game_id}, function(err, game) {
     if (err)
       res.send(err);
-
       var _letterAttempted = game[0].letterAttempted;
       Game.update({ _id: req.params.game_id },  { letterAttempted:  _letterAttempted+req.params.move  } ,function(err, game, raw) {
         if (err)
